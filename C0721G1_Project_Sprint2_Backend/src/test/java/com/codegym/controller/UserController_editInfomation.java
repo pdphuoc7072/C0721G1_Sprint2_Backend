93b7645fa6c6da04c52183874b56adc64e01ba8a
thanhbinhkd95 <81392431+thanhbinhkd95@users.noreply.github.com>
2022-01-18 16:29:34 +0700

    BinhNT update JUnit5 18-01-2022

---
+++ C0721G1_Project_Sprint2_Backend/src/test/java/com/codegym/controller/UserController_editInfomation.java
@@ -0,0 +1,252 @@
+package com.codegym.controller;
+
+import com.codegym.dto.UserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserController_editInfomation {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+    @Test
+    public void editUser_name_13() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName(null);
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("2000-10-16");
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editUser_birthDay_13() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyễn văn a");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday(null);
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editUser_phone_13() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyễn văn a");
+        userDto.setPhone(null);
+        userDto.setBirthday("2000-01-01");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");;
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editUser_address_13() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyễn văn a");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("2000-05-03");
+        userDto.setGender(0);
+        userDto.setAddress(null);
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void  editUser_name_14() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("2000-10-16");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editUser_birthDay_14() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyễn văn a");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editUser_phone_14() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyễn văn a");
+        userDto.setPhone("");
+        userDto.setBirthday("2000-01-01");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editUser_address_14() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyễn văn a");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("2000-05-03");
+        userDto.setGender(0);
+        userDto.setAddress("");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void  editUser_name_15() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyen van 123");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("2000-10-16");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editUser_birthDay_15() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyễn văn a");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("2000/01/01");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/admin/user/create")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editUser_phone_15() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyễn văn a");
+        userDto.setPhone("0985666777");
+        userDto.setBirthday("2000-01-01");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.post("/api/admin/user/create")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+
+    @Test
+    public void  editUser_name_16() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("a");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("2000-10-16");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void  editUser_name_17() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyen van aaaaaaaaaaaaaaaaaaaaaaaaa");
+        userDto.setPhone("0909999111");
+        userDto.setBirthday("2000-10-16");
+        userDto.setGender(0);
+        userDto.setAddress("Huế");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void  editUser_name_18() throws Exception {
+        UserDto userDto = new UserDto();
+        userDto.setId((long) 1);
+        userDto.setName("Nguyen van Ab");
+        userDto.setPhone("0909999999");
+        userDto.setBirthday("2000-10-16");
+        userDto.setGender(0);
+        userDto.setAddress("Đà Nẵng");
+        userDto.setImage("anh1.png");
+        this.mockMvc.perform(MockMvcRequestBuilders.patch("/api/user/update/{id}")
+                .content(this.objectMapper.writeValueAsString(userDto))
+                .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is2xxSuccessful());
+    }
+
+}
+
---
+++ C0721G1_Project_Sprint2_Backend/src/test/java/com/codegym/controller/UserController_editPassword.java
@@ -0,0 +1,126 @@
+package com.codegym.controller;
+
+import com.codegym.dto.UserDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class UserController_editPassword {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Test
+    public void editPassword_userName_19() throws Exception {
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId(1l);
+        userDto.setUserName(null);
+        userDto.setPassword("111111");
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/editPass")
+                        .content(this.objectMapper.writeValueAsString(userDto))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editPassword_userName_20() throws Exception {
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId(1l);
+        userDto.setUserName("");
+        userDto.setPassword("111111");
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/editPass")
+                        .content(this.objectMapper.writeValueAsString(userDto))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }    @Test
+    public void editPassword_password_21() throws Exception {
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId(1l);
+        userDto.setUserName("admin");
+        userDto.setPassword("++11__");
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/editPass")
+                        .content(this.objectMapper.writeValueAsString(userDto))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editPassword_password_22() throws Exception {
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId(1l);
+        userDto.setUserName("admin");
+        userDto.setPassword("1b");
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/editPass")
+                        .content(this.objectMapper.writeValueAsString(userDto))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editPassword_password_23() throws Exception {
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId(1l);
+        userDto.setUserName("admin");
+        userDto.setPassword("1b11111111111");
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/editPass")
+                        .content(this.objectMapper.writeValueAsString(userDto))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+    @Test
+    public void editPassword_24() throws Exception {
+
+        UserDto userDto = new UserDto();
+
+        userDto.setId(1l);
+        userDto.setPassword("tinh123");
+        userDto.setUserName("admin");
+
+        this.mockMvc
+                .perform(MockMvcRequestBuilders
+                        .post("/api/editPass")
+                        .content(this.objectMapper.writeValueAsString(userDto))
+                        .contentType(MediaType.APPLICATION_JSON_VALUE))
+                .andDo(print())
+                .andExpect(status().is4xxClientError());
+    }
+}
